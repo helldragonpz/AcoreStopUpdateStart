949553ad3765a72228c5f35b41642f1c3b7f85bd
helldragonpz <46687151+helldragonpz@users.noreply.github.com>
2023-01-24 21:01:12 +0200

    Update update.sh with new new functions.
    Added discord pm with log_file using Webhook Url
    Added loading % bar
    Added more functions
    Easy to config

--- update.sh
+++ update.sh
@@ -1,364 +1,213 @@
-#!/bin/bash
-###################
-#      Logs       #
-###################
-new=$(date +%d%m%y)
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/log/Update_$new.log 2>&1
-# Everything below will go to the file 'log.out':
+#!/usr/bin/env bash
 ##################
 #     Configs    #
 ##################
 [ -z $1 ] && time=300 || time=$1
 [ $time -gt 60 ] && timetext="$((time / 60)) minutes" || timetext="1 minute"
 
-echo "Time: $time"
-echo "Timetext: $timetext"
+chat="Server will be down for 30 mins after shutdown for maintenance!" #inform your player that your server entering maintenance!
+world1="/root/wow" #directory to your main realm
+world2="/root/wow2" #directory to your second realm
+sleeptime="320" #shoud be $time + 20 seks
+WEBHOOK="https://discord.com/api/webhooks/" #a discord configuration for WebHooks
 
-chat="Server will be down for 30 mins after shutdown for maintenance!" #inform your player that your server entering in maintancance!
-world1="/path/to/azerothcore realm1" #directory to your main realm
-world2="/path/to/world2 realm2" #directory to your second realm
+modules_world1=(
+#EDIT to your modules
+  "$world1/modules/mod-anticheat"
+  "$world1/modules/mod-multi-client-check"
+)
 
-############Realm - 1 Update##########
-# send key to stop Realm1 - Blizzlike#
-######################################
-tmux send-keys -t world-session ".nameannounce $chat" ENTER \; 
+modules_world2=(
+#EDIT to your modules
+  "$world2/modules/mod-account-achievements"
+  "$world2/modules/mod-anticheat"
+  "$world2/modules/mod-eluna"
+  "$world2/modules/mod-pvp-titles"
+  "$world2/modules/mod-autobalance"
+  "$world2/modules/mod-guildhouse"
+  "$world2/modules/mod-reward-played-time"
+  "$world2/modules/mod-cfbg"
+  "$world2/modules/mod-transmog"
+  "$world2/modules/mod-weekend-xp"
+  "$world2/modules/mod-ah-bot"
+  "$world2/modules/mod-duel-reset"
+  "$world2/modules/mod-multi-client-check"
+)
+
+# Function to send notification to the realm
+send_notification_world() {
+  tmux send-keys -t world ".nameannounce $chat" ENTER \; 
     if [[ $? -ne 0 ]]; then
-
+        echo "Aunnonceis not send!"
         exit $?
-        echo "Aunnonce is not send!"
     fi
-sleep 5
-        echo "Notification for maintenance sent successfully!"
+    echo "Notification for maintenance sent successfully!"
+}
 
-tmux send-keys -t world-session ".server shutdown $time" ENTER \;
-
-        echo "Realm will shutdown after $timetext!"
+send_notification_world2() {
+  tmux send-keys -t world2 ".nameannounce $chat" ENTER \;
     if [[ $? -ne 0 ]]; then
+        echo "Aunnonceis not send!"
         exit $?
-        echo "Realm2 is not shuting down check log files!"
     fi
-{
-  countdown=${1:-$time}   ## 360-second default (5mins)
-  w=${#countdown}
-  while [ $countdown -gt 0 ]
+    echo "Notification for maintenance sent successfully!"
+}
+
+# Function to update modules for the realm
+update_modules_world() {
+  for module in "${modules_world1[@]}"
   do
-    sleep 1 &
-    printf "  %${w}d\r" "$countdown"
-    countdown=$(( $countdown - 1 ))
-    wait
+    cd $module
+    git pull
+    if [[ $? -ne 0 ]]; then
+        echo "Git pull error, check log files!"
+        exit $?
+    fi
+    echo "Downloading updates for $module"
   done
-  printf "\a"
-} 2>/dev/null
+}
 
-    sleep 320 #Edit this if you change server shutdown time! Example 360 second to shutdown +10 sek = 370
-    tmux kill-session -t auth-session
-    tmux kill-session -t world-session
-        echo "Blizzlike realm sucksessfuly shutdown!"
-sleep 2
+update_modules_world2() {
+  for i in "${modules_world2[@]}"; do
+    cd "$i"
+    git pull
+    if [[ $? -ne 0 ]]; then
+        echo "Git pull error for module $(basename $i), check log files!"
+        exit $?
+    fi
+    echo "Downloading updates for module $(basename $i)!"
+  done
+}
+
+# Function to stop the realm
+stop_realm_world() {
+  tmux send-keys -t world ".server shutdown $time" ENTER \;
+    if [[ $? -ne 0 ]]; then
+        echo "Realm is not shutting down, check log files!"
+        exit $?
+    fi
+    echo "Realm will shutdown after $timetext!"
+     sleep_time=$sleeptime
+
+# Start sleep in background process
+sleep $sleep_time &
+pid=$!
+
+for ((i=1; i<=sleep_time; i++)); do
+    percentage=$(( (i*100)/sleep_time ))
+    printf "\rProgress: [%3d%%]" $percentage
+    sleep 1
+done
+
+# Wait for sleep process to finish
+wait $pid
+
+# Continue with the script
+echo "The script continues"
+    tmux kill-session -t auth
+    tmux kill-session -t world
+    echo "Blizzlike realm successfully shutdown!"
+}
+
+stop_realm_world2() {
+  tmux send-keys -t world2 ".server shutdown $time" ENTER \;
+    if [[ $? -ne 0 ]]; then
+        echo "Realm is not shutting down, check log files!"
+        exit $?
+    fi
+    echo "Realm will shutdown after $timetext!"
+    sleep_time=$sleeptime
+
+# Start sleep in background process
+sleep $sleep_time &
+pid=$!
+
+for ((i=1; i<=sleep_time; i++)); do
+    percentage=$(( (i*100)/sleep_time ))
+    printf "\rProgress: [%3d%%]" $percentage
+    sleep 1
+done
+
+# Wait for sleep process to finish
+wait $pid
+
+# Continue with the script
+echo "The script continues"
+    tmux kill-session -t world2
+    echo "Second realm successfully shutdown!"
+}
+
+###################
+#      Logs       #
+###################
+current_date=$(date +%Y-%m-%d)
+log_path="$HOME/wow/logs/" #Edit the Log folder "Need to be created beaufore you run the script"
+log_file="$log_path/update-$current_date.log"
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec &> >(tee -a "$log_file") 2>&1
+
+# Main script
+send_notification_world
+stop_realm_world
 cd $world1
-
-
 git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-sleep 5
-        echo "Downloading updates for Blizzlike Realm!"
-#Client data auto update
-set -e
-    ./acore.sh client-data
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Client Data update error check log files!"
-    fi
-sleep 5
-        echo "Downloading Client Data updates for Blizzlike Realm!"
-##############################
-#Git Pull Modules auto update#
-##############################
-#Anticheat
-cd $world1/modules/mod-anticheat
-
-git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-sleep 5
-        echo "Downloading updates for Anticheat!"
-
-#Multi Client Check
-cd $world1/modules/mod-multi-client-check
-
-git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-sleep 5
-        echo "Downloading updates for Multi Client Check!"
-
-#Cmake Compiler
-cd world1
+if [[ $? -ne 0 ]]; then
+    echo "Git pull error, check log files!"
+    exit $?
+fi
+echo "Downloading updates for Blizzlike realm!"
+./acore.sh client-data
+if [[ $? -ne 0 ]]; then
+    echo "Client Data update error, check log files!"
+    exit $?
+fi
+echo "Downloading Client Data updates for Blizzlike realm!"
+update_modules_world
+cd $world1
 ./acore.sh compiler build
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Build updates error check log files!"
-    fi
-sleep 5
-        echo "Build updates finished!"
+if [[ $? -ne 0 ]]; then
+    echo "Build updates error, check log files!"
+    exit $?
+fi
+echo "Build updates finished!"
+cd $world1
+./start.sh
 
-#Auto Start Server
-
-./world.sh
-    if [[ $? -ne 0 ]]; then
-            exit $?
-        fi
-    sleep 30
-echo "Blizzlike realm updated and started use 'tmux attach -t world-session!' to attach it!"
-
-#Realm - 2 Auto Update
-cd world2
-
-###############################
-# send key to stop Realm2 - 5x#
-###############################
-tmux send-keys -t world-session2 ".nameannounce $chat" ENTER \;
- if [[ $? -ne 0 ]]; then
-                        exit $?
-                        echo "Aunnonce is not send!"
-                fi
-sleep 1
-                        echo "Notification for maintenance sent successfully!"
-
-tmux send-keys -t world-session2 ".server shutdown $time" ENTER \;
-
-        echo "Realm will shutdown after $timetext!"
-	if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Realm2 is not shuting down check log files!"
-        fi
-{
-  countdown=${1:-$time}   ## 360-second default (5mins)
-  w=${#countdown}
-  while [ $countdown -gt 0 ]
-  do
-    sleep 1 &
-    printf "  %${w}d\r" "$countdown"
-    countdown=$(( $countdown - 1 ))
-    wait
-  done
-  printf "\a"
-} 2>/dev/null
-
-    sleep 320 #Edit this if you change server shutdown time! Example 360 second to shutdown +10 sek = 370
-
-    tmux kill-session -t world-session2
-        echo "Realm Zul 5x sucksessfuly shutdown!"
-
-######################
-#Git Pull Auto update#
-######################
-    git pull
-    if [[ $? -ne 0 ]]; then
-           exit $?
-           echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Zul  5x Realm!"
-#########################
-#Client data auto update#
-#########################
-set -e
-    ./acore.sh client-data
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Client Data update error check log files!"
-    fi
-        sleep 5
-			echo "Downloading Client Data updates for Zul 5x Realm!"
-##############################
-#Git Pull Modules auto update#
-##############################
-#Account Achivment
-cd /home/tanados/world2/modules/mod-account-achievements
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-                        echo "Git pull error check log files!"
-    fi
-                sleep 5
-                        echo "Downloading updates for Account Achivment!"
-
-#Anticheat
-cd $world2/modules/mod-anticheat
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Anticheat!"
-
-#Eluna Engine
-cd $world2/modules/mod-eluna
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Eluna Engine!"
-
-#PVP Titles
-cd $world2/modules/mod-pvp-titles
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for PVP Titles!"
-
-#Auto Balance
-cd $world2/modules/mod-autobalance
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Auto Balance!"
-#GuildHouse
-cd $world2/modules/mod-guildhouse
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for GuildHouse!"
-#Reward Player Time
-cd $world2/modules/mod-reward-played-time
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Reward Player Time	!"
-
-#CFBG
-cd $world2/modules/mod-cfbg
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for CFBG!"
-#Auto Learn Spells
-cd $world2/modules/mod-learn-spells
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Auto Learn Spells!"
-#Transmog
-cd $world2/modules/mod-transmog
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Transmog!"
-#Weekend XP
-cd $world2/modules/mod-weekend-xp
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for Weekend XPt!"
-#AH Bot
-cd $world2/modules/mod-ah-bot
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-        sleep 5
-        echo "Downloading updates for AH Bot!"
-#Duel Reset
-cd $world2/modules/mod-duel-reset
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-		sleep 5
-        echo "Downloading updates for Duel Reset!"
-
-#Multi Client Check
-cd $world2/modules/mod-multi-client-check
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-		sleep 5
-        echo "Downloading updates for Multi Client Check!"
-#WorGoblin
-cd $world2/modules/mod-worgoblin
-
-    git pull
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Git pull error check log files!"
-    fi
-		sleep 5
-        echo "Downloading updates for WorGoblin!"  
-
-################
-#Cmake Compiler#
-################
-cd world2
-    ./acore.sh compiler build
-    if [[ $? -ne 0 ]]; then
-        exit $?
-        echo "Build updates error check log files!"
-    fi
-        sleep 5
-        echo "Build updates finished!"
-
-#Auto Start Server
-
-    ./world.sh
-    if [[ $? -ne 0 ]]; then
-            exit $?
-        fi
-    sleep 30
-
-echo "Zul 5x realm updated and started use 'tmux attach -t world-session!' to attach it!"
+# Second realm updates
+send_notification_world2
+stop_realm_world2
+cd $world2
+git pull
+if [[ $? -ne 0 ]]; then
+    echo "Git pull error, check log files!"
+    exit $?
+fi
+echo "Downloading updates for the second realm!"
+./acore.sh client-data
+if [[ $? -ne 0 ]]; then
+    echo "Client Data update error, check log files!"
+    exit $?
+fi
+echo "Downloading Client Data updates for the second realm!"
+update_modules_world2
+cd $world2
+./acore.sh compiler build
+if [[ $? -ne 0 ]]; then
+    echo "Build updates error, check log files!"
+    exit $?
+fi
+echo "Build updates finished!"
+cd $world2
+./start.sh
+sleep 60
+###############
+##  DISCORD  ##
+###############
+cd ~/wow/discord
+./discord.sh \
+  --webhook-url=$WEBHOOK \
+  --file $log_file \
+  --username "The Watcher" \
+  --text "This is an update for $current_date\nPlece take a look for errors!"
 
